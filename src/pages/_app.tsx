7ee2bcb51b906091375c49113d487f43c71d295c
qazal <ghazal.nzhrzr@gmail.com>
2022-05-06 18:17:06 +0430

    feat(theme): design system✨

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import "@fontsource/lato";
+import "@fontsource/raleway";
+
+import { theme } from "@astra/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
