07c791dc0c0087cecffe3c7f8cfbfe1278f85b4a
qazal <ghazal.nzhrzr@gmail.com>
2022-05-13 17:54:12 +0400

    refactore(pages): remove moralis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { MoralisProvider } from "react-moralis";
 
 import "@fontsource/lato";
 import "@fontsource/raleway";
@@ -8,18 +7,14 @@ import { theme } from "@astra/theme";
 import AdaptivityProvider from "@astra/providers/AdaptivityProvider";
 
 function MyApp({ Component, pageProps }) {
-  const SERVER_URL = "https://p99gmxhqipdu.usemoralis.com:2053/server";
-  const APP_ID = "LowLESoS9aXr5AK9f9JnWKgYlabyUvMChCvlN1dl";
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page: unknown) => page);
 
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <MoralisProvider serverUrl={SERVER_URL} appId={APP_ID}>
-        <AdaptivityProvider>
-          {getLayout(<Component {...pageProps} />)}
-        </AdaptivityProvider>
-      </MoralisProvider>
+      <AdaptivityProvider>
+        {getLayout(<Component {...pageProps} />)}
+      </AdaptivityProvider>
     </ChakraProvider>
   );
 }
